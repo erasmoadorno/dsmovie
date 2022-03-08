f980873b024038dcb518ef778a6a5e0ef4d9d2bc
Erasmo Bertuci Adorno <45499493+erasmoadorno@users.noreply.github.com>
2022-03-08 19:56:07 -0300

    MovieCard

---
+++ frontend/src/components/movieStar/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as StarFull} from 'assets/imgs/star-full.svg';
+import {ReactComponent as StarHalf} from 'assets/imgs/star-half.svg';
+import {ReactComponent as StarEmpty} from 'assets/imgs/star-empty.svg';
+
+import './style.css'
+
+function MovieStars() {
+  return (
+    <div className="dsmovie-stars-container">
+      <StarFull />
+      <StarFull />
+      <StarFull />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  );
+}
+
+export default MovieStars; 
